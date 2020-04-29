836c40d0a6a5f5ea1adffafa5e4407ed72fabee0
Lee <you@example.com>
2020-04-29 19:45:56 +0900

    쿠키런

--- src/cookierun/CookierunSwing.java
+++ src/cookierun/CookierunSwing.java
@@ -1,6 +1,7 @@
 package cookierun;
 
 import java.awt.Component;
+
 import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -15,20 +16,31 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class CookierunSwing extends JFrame {
-	private CookiePanel cp = new CookiePanel();
+	private CookiePanel cp;
+
+	ImageIcon backIc = new ImageIcon("img/bg.png");
+	Image backImg = backIc.getImage();
+	Image backImg2 = backIc.getImage();
+
+	ImageIcon human = new ImageIcon("img/finn.gif");
+	Image finn = human.getImage();
+
+	int backX = 0;
+	int backX2 = backImg.getWidth(null);
+	int humanPosition = 200;
 
 	public CookierunSwing() {
 		setTitle("Cookierun");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		cp = new CookiePanel();
 		setContentPane(cp);
 
-		setSize(500, 300);
+		setSize(700, 500);
 		setVisible(true);
 	}
 
 	class CookiePanel extends JPanel {
-		int imgX = 0;
-		int imgY = 0;
 
 		public CookiePanel() {
 			setFocusable(true);
@@ -37,40 +49,75 @@ public class CookierunSwing extends JFrame {
 				public void keyPressed(KeyEvent e) {
 					System.out.println("keycod :" + e.getKeyCode());
 					System.out.println("keychar :" + e.getKeyChar());
-					
+					if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+						new Thread(new Runnable() {
+
+							@Override
+							public void run() {
+								int a = 100;
+								while (a > 0) {
+
+									if (a >= 50) {
+										humanPosition = humanPosition - 5;
+									} else
+										humanPosition = humanPosition + 5;
+									a = a - 1;
+									
+									try {
+										Thread.sleep(10);
+									} catch (Exception e2) {
+										e2.printStackTrace();
+									}
+								}
+
+							}
+						}).start();
+					}
+
 				}
 			});
+
+			new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					while (true) {
+						backX = backX - 10;
+						backX2 = backX2 - 10;
+
+						if (backX < -(backImg.getWidth(null))) {
+							System.out.println("good");
+							backX = backImg.getWidth(null) - 1;
+						}
+						if (backX2 < -(backImg.getWidth(null))) {
+							System.out.println("good");
+							backX2 = backImg.getWidth(null) - 1;
+						}
+						repaint();
+
+						try {
+							Thread.sleep(50);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+
+				}
+			}).start();
+
 		}
 
 		@Override
 		protected void paintComponent(Graphics g) {
-
 			super.paintComponent(g);
-			ImageIcon imgBlack = new ImageIcon("img/black.png");
-			ImageIcon imgRunning = new ImageIcon("img/running.gif");
 
-			Image black = imgBlack.getImage();
-			Image running = imgRunning.getImage();
-			g.drawImage(black, imgX, 0, this.getWidth(), this.getHeight(), this);
+			g.drawImage(backImg, backX, 0, this);
+			g.drawImage(backImg, backX2+1, 0, this);
+			g.drawImage(backImg, backX2-1, 0, this);
+			g.drawImage(finn, 250, humanPosition, 200, 200, this);
 
-			g.drawImage(running, 120, 100, 50, 50, this);
-		
-			
-			
-			
-			
-		
-			
-			
 		}
-		public void actionPerformed(ActionEvent e) {
-			if(imgX < 0 || imgX> 500) {
-				imgX = imgX - 10;
-			}
-			repaint();
-		}
-	
-	
+
 	}
 
 	public static void main(String[] args) {
